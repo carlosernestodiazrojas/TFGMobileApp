523738feb98de0d0506cec0a400197b37e218da3
Carlos Ernesto Diaz Rojas <carlos.diaz@hotelverse.tech>
2025-07-08 19:38:15 +0100

    feature: special assessment dynamic data
    added support for dynamic datafrom nestjs api. Created special assessment details screen

---
+++ src/actions/specialAssessmentActions.ts
@@ -0,0 +1,24 @@
+
+
+import { SpecialAssessment, User } from "@/types";
+import type { ApiResponse } from "@/types/apiResponse";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { holaVecinoApi } from "../api/holaVecinoApi";
+
+export const getSpecialAssessments = async () => {
+
+    try {
+
+        const user = JSON.parse(await AsyncStorage.getItem("userData") as string) as User;
+
+        const response = await holaVecinoApi.get<ApiResponse<SpecialAssessment[]>>(`/special-assessments/allByHoa/${user.hoaId}`)
+        const { data } = response.data;
+
+        return data;
+
+    } catch (error) {
+        return []
+    }
+
+}
+
---
+++ src/screens/special-assessments/SpecialAssessmentDetailScreen.tsx
@@ -0,0 +1,77 @@
+
+import { Ionicons } from '@expo/vector-icons';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import React from 'react';
+import {
+    Image,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors, detailStyles, globalStyles } from '../../styles';
+import { RootStackParamList } from '../../types';
+
+import { formatDate } from '@/utils/formatDatesUtil';
+
+type SpecialAssessmentDetailScreenRouteProp = RouteProp<RootStackParamList, 'SpecialAssessmentDetail'>;
+
+export default function SpecialAssessmentDetailScreen() {
+    const navigation = useNavigation();
+    const route = useRoute<SpecialAssessmentDetailScreenRouteProp>();
+    const { specialAssessment } = route.params;
+
+    return (
+        <SafeAreaView style={globalStyles.container}>
+
+            <View style={[globalStyles.flexRowBetween, globalStyles.header]}>
+                <TouchableOpacity
+                    style={{ padding: 8 }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Ionicons name="arrow-back" size={24} color={colors.primary} />
+                </TouchableOpacity>
+                <Text style={globalStyles.headerSubtitle}>Detalle de la derrama</Text>
+                <TouchableOpacity style={{ padding: 8 }}>
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView style={globalStyles.flex1}>
+
+                <Image
+                    source={{ uri: specialAssessment.imagesUrls[0] }}
+                    style={globalStyles.imageDetail}
+                />
+
+                <View style={detailStyles.detailsContainer}>
+                    <Text style={globalStyles.titleDark}>{specialAssessment.title}</Text>
+                    <Text style={[globalStyles.textDateLarge, globalStyles.marginBottomLg]}>
+                        Creado: {formatDate(specialAssessment.created)}
+                    </Text>
+
+                    <View style={[detailStyles.infoRow]}>
+                        <Text style={detailStyles.sectionTitle}>Monto total: {specialAssessment.total_amount}</Text>
+                    </View>
+
+                    <View style={[detailStyles.infoRow, { marginBottom: 20 }]}>
+                        <Text style={detailStyles.sectionTitle}>Monto individual: {specialAssessment.individual_amount}</Text>
+                    </View>
+
+                    <Text style={detailStyles.sectionTitle}>Información</Text>
+                    <Text style={detailStyles.description}>{specialAssessment.description}</Text>
+
+                    <View style={detailStyles.contactContainer}>
+                        <Text style={[globalStyles.textSmall, { color: colors.gray500 }]}>
+                            ID de la derrama
+                        </Text>
+                        <Text style={[globalStyles.textBold, { fontSize: 18, color: colors.gray800 }]}>
+                            {specialAssessment.id}
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
+    );
+}
